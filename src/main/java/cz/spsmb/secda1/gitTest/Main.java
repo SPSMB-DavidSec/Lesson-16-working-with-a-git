3dbba6d8860869ca6509a03ebf5f37ef66c3dd3c
sec <sec@spsmb.cz>
2023-12-20 11:33:05 +0100

    Task2 - implemented second power of number.

--- src/main/java/cz/spsmb/secda1/gitTest/Main.java
+++ src/main/java/cz/spsmb/secda1/gitTest/Main.java
@@ -2,6 +2,6 @@ package cz.spsmb.secda1.gitTest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World.");
     }
 }
